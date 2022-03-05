f6a7a853de13eedb36a8a1aaa95e631166aad544
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-04 23:53:58 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
 }
